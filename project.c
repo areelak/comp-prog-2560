626774cd5623719448026289bb42675695539a81
Areelak Treesuwan <areelak.t@ku.th>
2018-01-24 21:21:42 +0700

    add project file

---
+++ project.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+	int minutes1, days, hours, minutes;
+	printf("Enter computer working time: ");
+	scanf("%d", &minutes);
+	days = minutes/1440;
+	hours = (minutes%1440)/60;
+	minutes1 = ((minutes%1440)%60);
+	printf("It is %d days %d hours and %d minutes.\n", days, hours, minutes1 );
+}
